bed3f4d4e958c5e1f394ca4ef5bb30fffc67bea5
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-02-10 17:55:35 -0300

    test: testing 'Float Point Math'

---
+++ src/TestaDouble.java
@@ -0,0 +1,9 @@
+
+public class TestaDouble {
+	public static void main(String[] args) {
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println(total);
+	}
+}
